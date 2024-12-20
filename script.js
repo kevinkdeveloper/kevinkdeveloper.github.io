bb829befa7501a4b57ce6fae43de5be43e00c04e
kevinkgithub <120767578+kevinkdeveloper@users.noreply.github.com>
2024-12-20 08:34:47 +0800

    engineering finance design

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    let scrollPosition = window.scrollY; // Get the scroll position
-    let header = document.querySelector('header');
-  
-    // Adjust the opacity based on the scroll position
-    let opacity = Math.min(scrollPosition / 500, 0.7); // Limit max opacity to 0.7
-    header.style.backgroundColor = `rgba(0, 0, 0, ${0.5 + opacity})`;
-  });
-  
